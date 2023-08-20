29e9df990fdc97f119afcaeeca9671e2ad8f9760
RubieStannard <126431815+RubieStannard@users.noreply.github.com>
2023-08-20 10:00:17 +1000

    Add files via upload

---
+++ Visualisation Project/javascript/afghanistan_radar.js
@@ -0,0 +1,203 @@
+// ******* Common varaible set up *******
+var year = ["2021", "2020", "2019", "2018"];
+var w = 600;
+var h = 600;
+var data = [
+  {
+    className: "conflict-and-violence",
+    axes: [
+      { axis: "2021", value: 1.2967949 },
+      { axis: "2020", value: 0.2047571 },
+      { axis: "2019", value: -0.477767 },
+      { axis: "2018", value: -1.023785 }
+    ]
+  },
+  {
+    className: "natural-disaster",
+    axes: [
+      { axis: "2021", value: -0.838111 },
+      { axis: "2020", value: -0.179154 },
+      { axis: "2019", value: 1.4435281 },
+      { axis: "2018", value: -0.426263 }
+    ]
+  },
+  {
+    className: "human-trafficking",
+    axes: [
+      { axis: "2021", value: 0.4524736 },
+      { axis: "2020", value: 1.1694712 },
+      { axis: "2019", value: -0.574063 },
+      { axis: "2018", value: -1.047882 }
+    ]
+  }
+];
+
+
+
+
+// ******* Chart varaible set up *******
+var padding = 40; 
+var containerSize = Math.min(w, h) - padding * 2;
+var scale = d3.scaleLinear().domain([0, 10]).range([0, containerSize / 2]);
+var ticks = [2, 4, 6, 8, 10];
+var max = d3.max(data, d => d3.max(d.axes, d => d.value));
+var Cscale = d3.scaleLinear().domain([0, max]).range([0, containerSize / 2]);
+var svg = d3.select("#chart").append("svg").attr("width", w).attr("height", h);
+
+// ******* Chart code: grid *******
+const angle = (2 * Math.PI) / year.length; // Angle between each point of the polygon
+const centerX = w / 2;
+const centerY = h / 2;
+const polygonVertices = year.map((d, i) => [
+  centerX + Math.cos(i * angle + Math.PI / 2) * scale(10),
+  centerY + Math.sin(i * angle + Math.PI / 2) * scale(10)
+]);
+polygonVertices.push(polygonVertices[0]);
+svg.append("polygon").attr("points", polygonVertices.map(d => d.join(",")).join(" "));
+
+
+// ******* Chart code: grid lines *******
+ticks.forEach((tick) => {
+  year.forEach((d, i) => {
+    const x1 = centerX + Math.cos(i * angle + Math.PI / 2) * scale(tick);
+    const y1 = centerY + Math.sin(i * angle + Math.PI / 2) * scale(tick);
+    const x2 = centerX + Math.cos((i + 1) * angle + Math.PI / 2) * scale(tick);
+    const y2 = centerY + Math.sin((i + 1) * angle + Math.PI / 2) * scale(tick);
+
+    svg.append("line")
+      .attr("x1", x1)
+      .attr("y1", y1)
+      .attr("x2", x2)
+      .attr("y2", y2)
+
+  });
+});
+
+// ******* Chart code: grid axis *******
+function angleToCoordinate(angle, point) {
+  var x = centerX + Math.cos(angle - Math.PI / 2) * scale(10);
+  var y = centerY + Math.sin(angle - Math.PI / 2) * scale(10);
+  return { "x": x, "y": y };
+}
+
+var YearData = year.map((f, i) => {
+  var angle = (Math.PI / 2) + (2 * Math.PI * i / year.length) + (Math.PI / 2);
+  var labelDistance = scale(10);
+  var labelCoord = angleToCoordinate(angle, labelDistance);
+  return {
+    "name": f,
+    "angle": angle,
+    "line_coord": angleToCoordinate(angle, 10),
+    "label_coord": labelCoord
+  };
+});
+
+svg.selectAll(".axislabel")
+  .data(YearData)
+  .enter()
+  .append("text")
+  .attr("class", "axislabel")
+  .attr("x", d => d.label_coord.x)
+  .attr("y", d => d.label_coord.y)
+  .text(d => d.name);
+
+
+
+// ******* Chart code: Legend *******
+// Legend varaibles
+var width = 350
+var height = 200
+var Svg = d3.select("#ledged").append("svg").attr("width", width).attr("height", height)
+var keys = ["CONFLICT AND VIOLENCE", "NATURAL DISASTER", "HUMAN TRAFFICING"]
+var colour = ["rgba(124, 97, 108, 1)", "rgba(221, 202, 217, 1)", "rgba(170, 189, 223, 1)"]
+
+// Ledend code 
+Svg.selectAll("mydots")
+  .data(keys)
+  .enter()
+  .append("circle")
+  .attr("cx", 100)
+  .attr("cy", function (d, i) { return 100 + i * 25 }) // 100 is where the first dot appears. 25 is the distance between dots
+  .attr("r", 7)
+  .style("fill", function (d, i) { return colour[i] })
+
+Svg.selectAll("mylabels")
+  .data(keys)
+  .enter()
+  .append("text")
+  .attr("x", 120)
+  .attr("y", function (d, i) { return 100 + i * 25 }) // 100 is where the first dot appears. 25 is the distance between dots
+  .style("fill", function (d, i) { return colour[i] })
+  .text(function (d) { return d })
+  .attr("text-anchor", "left")
+  .style("alignment-baseline", "middle")
+
+  
+// ******* Chart code: Drawing Data*******
+  var line = d3.lineRadial().curve(d3.curveCardinalClosed).radius(d => Cscale(d.value)).angle((d, i) => i * angle);
+  var g = svg.append("g").attr("transform", `translate(${centerX},${centerY})`);
+// drawing chart data 
+var blobWrapper = g.append("g")
+  .attr("class", "all-blobs-wrapper")
+  .selectAll(".blob-wrapper")
+  .data(data)
+  .enter().append("g")
+  .attr("class", d => "blob-wrapper" + d.className);
+
+blobWrapper.append("path")
+  .attr("class", d => "blob-area blob-area-" + d.className)
+  .attr("d", (d, i) => {
+    return line(d.axes);
+  })
+// ******* Chart interactity: opacity popout of data *******
+  .on('mouseover', function (d, i) {
+    d3.selectAll(".blob-area")
+      .transition().duration(200)
+      .style("fill-opacity", 0.3);
+    d3.select(this)
+      .transition().duration(200)
+      .style("fill-opacity", 0.8);
+  })
+  .on('mouseout', function () {
+    d3.selectAll(".blob-area")
+      .transition().duration(200)
+      .style("fill-opacity", 0.5);
+  })
+  .style("fill", (d, i) => colour[i])
+  .style("fill-opacity", 0.5)
+  .style("stroke", (d, i) => colour[i])
+  .style("stroke-width", 2);
+
+// ******* Chart interactity: tool tip *******
+blobWrapper.append("path")
+var blobWrapper = g.append("g")
+  .attr("class", "all-blobs-wrapper")
+  .selectAll(".blob-wrapper")
+  .enter().append("g")
+  .attr("class", d => "blob-wrapper" + d.className);
+
+blobWrapper.append("path")
+  .attr("class", d => "blob-area blob-area-" + d.className)
+  .attr("d", (d, i) => {
+    return line(d.axes);
+  }).on("mouseover", function(d) {
+    // Get the class name of the current blob area
+    var className = d.className;
+    // Show the corresponding tooltip based on the class name
+    d3.select("#" + className).classed("hidden", false);
+  })
+  .on("mouseout", function(d) {
+    // Get the class name of the current blob area
+    var className = d.className;
+    // Hide the corresponding tooltip based on the class name
+    d3.select("#" + className).classed("hidden", true);
+  })
+  
+
+
+ 
+
+
+
+
+
---
+++ Visualisation Project/javascript/legal_choropleth.js
@@ -0,0 +1,133 @@
+// D3 code goes here
+// create SVG element
+const svg = d3.select("svg"),
+  width = svg.attr("width"), // using variables to specify width of SVG
+  height = svg.attr("height"), // using variables to specify height of SVG
+  path = d3.geoPath(),
+  data = d3.map(),
+  worldmap = "countries.geo.json",
+  worldpopulation = "legal_data.csv";
+    
+let centered, world;
+
+// style the geographic projection and scaling
+const projection = d3.geoRobinson()
+                  .scale(130)
+                  .translate([width / 2, height / 2]);
+    
+// definie the colour scale
+const colorScale = d3.scaleThreshold()
+                  .domain([10000, 80000, 100000, 200000, 300000, 500000, 1000000, 5000000])
+                  .range(["#d3d3d3","#bfe2db","#a2d9ce","#84cfc1","#4ba8c9","#2989bd","#0a6aad","#254b8c"]);
+    
+// add the tooltip
+const tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip")
+                .style("opacity", 0);
+
+// load the csv data and boot
+d3.queue()
+  .defer(d3.json, worldmap)
+  .defer(d3.csv, worldpopulation, function(d) {data.set(d.code, + d.total);})
+  .await(ready);
+    
+// create the choropleth drawing
+function ready(error, topo) { // topo is the data received from the d3.queue function (world.geojson)
+    
+let mouseOver = function(d) {
+  
+d3.selectAll(".Country")
+  .transition()
+  .duration(200)
+  .style("opacity", .5)
+  .style("stroke","transparent");
+        
+d3.select(this)
+  .transition()
+  .duration(200)
+  .style("opacity", 1)
+  .style("stroke", "black");
+  
+tooltip.style("left", (d3.event.pageX + 15) + "px")
+    .style("top", (d3.event.pageY - 28) + "px")
+    .transition().duration(400)
+    .style("opacity", 1)
+    .text(d.properties.name + ': ' + d.total);}
+    
+let mouseLeave = function() {
+    
+d3.selectAll(".Country")
+  .transition()
+  .duration(200)
+  .style("opacity", 1)
+  .style("stroke", "transparent");
+      
+tooltip.transition().duration(300)
+    .style("opacity", 0);}
+    
+// draw the outline for the world map
+world = svg.append("g")
+        .attr("class", "world");
+          
+world.selectAll("path")
+  .data(topo.features)
+  .enter()
+  .append("path")
+  .attr("d", d3.geoPath().projection(projection)) // drawing the outline for each country using d3.geoPath(), a built-in function that takes care of showing the map from a properly formatted geojson file
+  .attr("data-name", function(d) {return d.properties.name}) //retrieve the name of the country from data
+  .attr("fill", function(d) {d.total = data.get(d.id);return colorScale(d.total);}) // set the color of each country
+  .style("stroke", "transparent")
+  .attr("class", function(d) {return "Country"}) // add a class, styling and mouseover/mouseleave and click functions
+  .attr("id", function(d) {return d.id})
+  .style("opacity", 1)
+  .on("mouseover", mouseOver)
+  .on("mouseleave", mouseLeave)
+  .on("click", click);
+      
+// create the legend
+const x = d3.scaleLinear()
+          .domain([0, 7])
+          .range([10000, 80000, 100000, 200000, 300000, 500000, 1000000, 5000000]);
+    
+const legend = svg.append("g")
+                .attr("id", "legend");
+    
+const legend_entry = legend.selectAll("g.legend")
+                      .data(colorScale.range().map(function(d) {d = colorScale.invertExtent(d); 
+                        if (d[0] == null) d[0] = x.domain()[0]; if (d[1] == null) d[1] = x.domain()[1]; return d;}))
+                      .enter().append("g")
+                      .attr("class", "legend_entry");
+    
+const ls_w = 20,
+      ls_h = 20;
+    
+legend_entry.append("rect")
+        .attr("x", 20)
+        .attr("y", function(d, i) {return height - (i * ls_h) - 2 * ls_h;})
+        .attr("width", ls_w)
+        .attr("height", ls_h)
+        .style("fill", function(d) {return colorScale(d[0]);})
+        .style("opacity", 0.8);
+    
+legend_entry.append("text")
+        .attr("x", 50)
+        .attr("y", function(d, i) {return height - (i * ls_h) - ls_h - 6;})
+        .text(function(d, i) {
+          if (i === 0) return "< " + d[1];
+          if (i === 1) return "10000 - " + d[1];
+          if (i === 2) return "80000 - " + d[1];
+          if (i === 3) return "100000 - " + d[1];
+          if (i === 4) return "200000 - " + d[1];
+          if (i === 5) return "300000 - " + d[1];
+          if (i === 6) return "500000 - " + d[1];
+          if (i === 7) return "1000000 - " + d[1];});
+    
+legend.append("text").attr("x", 15).attr("y", 280);}
+    
+// legend position
+function click(d) {
+  var x, y, k; 
+  
+world.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });}
